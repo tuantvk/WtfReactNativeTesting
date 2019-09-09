07f95d62ad6c4ef81f2a4556adc9e1be93f029e9
tuantv <tuanhanhphuc123@gmail.com>
2019-09-09 22:50:57 +0700

    update Button

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   TouchableOpacity,
   Text,
 } from 'react-native';
 
-const Button = ({ onPress, label }) => {
+const Button = ({ label }) => {
+
+  const [disabled, _setDisabled] = useState(false);
+
+  _onSubmit = () => {
+    // Do something...
+
+    _setDisabled(true);
+
+  }
+
   return (
-    <TouchableOpacity onPress={onPress}>
+    <TouchableOpacity
+      disabled={disabled}
+      onPress={_onSubmit}>
       <Text>
-        {label}
+        {disabled ? 'Loading...' : label}
       </Text>
     </TouchableOpacity>
   );
--- src/Button/Button.spec.js
+++ src/Button/Button.spec.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { render, fireEvent } from 'react-native-testing-library';
+import renderer from 'react-test-renderer';
 import Button from './Button';
 
 describe('Button', () => {
 
   describe('Rendering', () => {
     it('should match to snapshot', () => {
-      const component = shallow(<Button label="Click me" />)
+      const component = shallow(<Button label="Submit" />)
       expect(component).toMatchSnapshot()
     });
+
+    it('Button renders correctly', () => {
+
+      const tree = renderer.create(<Button />).toJSON();
+      expect(tree).toMatchSnapshot();
+
+    });
   });
+
+  describe('onPress', () => {
+    it('on submit login', () => {
+
+      const { queryByText, getByTestId } = render(
+        <Button />
+      );
+
+      fireEvent.press(getByTestId('btnLogin'));
+
+      expect(queryByText('Loading...')).not.toBeNull();
+    });
+
+  })
 
 });
